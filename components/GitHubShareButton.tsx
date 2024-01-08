1e1e59bcd05bbf3a28f1b53d3f81d4cee865a6b1
Simeon Bowman <134252285+DOJO-MNKY@users.noreply.github.com>
2024-01-08 14:20:36 -0500

    Update GitHubShareButton.tsx

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -16,7 +16,7 @@ export const GitHubShareButton: React.FC = () => {
         height='80'
         viewBox='0 0 250 250'
         style={{
-          fill: '#C3061A',
+          fill: '#46604A',
           color: '#fff',
           position: 'absolute',
           zIndex: 1001,
